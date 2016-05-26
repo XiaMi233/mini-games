6227bd7af92a52ad4e6bd7c7c69a0d1ca6b6d5d9
kandamashiro <641047675@qq.com>
2016-05-26 15:13:26 +0800

    finish angry meteor game good job!

---
+++ Angry Meteor/Assets/Scripts/TargetDamage.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetDamage : MonoBehaviour {
+
+    public int hitPoints = 2;
+    public Sprite damagedSprite;
+    public float damageImpactSpeed;
+
+    private int currentHitPoints;
+    private float damageImpactSpeedSqr;
+    private SpriteRenderer _spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        currentHitPoints = hitPoints;
+        damageImpactSpeedSqr = Mathf.Sqrt(damageImpactSpeed);
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.collider.tag != "Damager") {
+            return;
+        }
+
+        if (collision.relativeVelocity.sqrMagnitude < damageImpactSpeedSqr) {
+            return;
+        }
+
+        _spriteRenderer.sprite = damagedSprite;
+        --currentHitPoints;
+
+        if (currentHitPoints <= 0) {
+            kill();
+        }
+    }
+
+    void kill() {
+        _spriteRenderer.enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<Rigidbody2D>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+}
