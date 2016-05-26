d4e122a79ece44237996fb46813bf8f7be648ffc
kandamashiro <641047675@qq.com>
2016-05-26 13:10:30 +0800

    add camare following and game reset

---
+++ Angry Meteor/Assets/Scripts/ProjectileFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileFollow : MonoBehaviour {
+
+    public Transform projectile;
+    public Transform farLeft;
+    public Transform farRight;
+
+    private Transform _transfrom;
+    void Awake() {
+        _transfrom = GetComponent<Transform>();
+    }
+
+	void Update () {
+        Vector3 newPosition = transform.position;
+        newPosition.x = Mathf.Clamp(projectile.position.x, farLeft.position.x, farRight.position.x);
+        _transfrom.position = newPosition;
+	}
+}
---
+++ Angry Meteor/Assets/Scripts/Reseter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Reseter : MonoBehaviour {
+
+    public Rigidbody2D projectile;
+    public float resetSpeed = 0.025f;
+
+    private float resetSpeedSqr;
+    private SpringJoint2D spring;
+
+    // Use this for initialization
+    void Awake() {
+        resetSpeedSqr = Mathf.Sqrt(resetSpeed);
+        spring = projectile.GetComponent<SpringJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Reset();
+        }
+
+        if (spring == null && projectile.velocity.sqrMagnitude < resetSpeedSqr) {
+            Reset();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.GetComponent<Rigidbody2D>() == projectile) {
+            Reset();
+        }
+    }
+
+    void Reset() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
