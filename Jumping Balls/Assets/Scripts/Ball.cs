d7138ad1deeb9c4e7390f5cd99c08dbfafdb5be4
kandamashiro <641047675@qq.com>
2016-06-27 02:14:53 +0800

    finish jumping balls

---
+++ Jumping Balls/Assets/Scripts/Ball.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    private Rigidbody rdbody;
+
+	// Use this for initialization
+	void Start () {
+        rdbody = GetComponent<Rigidbody>();
+        rdbody.velocity = new Vector3(-10.0f, 9.0f, 0.0f);	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
---
+++ Jumping Balls/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    protected float jumpSpeed = 12.0f;
+
+    private Rigidbody rdbody;
+
+    [SerializeField]
+    private bool isLanding = false;
+
+	// Use this for initialization
+	void Start () {
+        rdbody = GetComponent<Rigidbody>();
+        isLanding = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isLanding) {
+            if (Input.GetMouseButtonDown(0)) {
+                isLanding = false;
+                rdbody.velocity = Vector3.up * this.jumpSpeed;
+            }
+        }	
+	}
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Floor") {
+            isLanding = true;
+        }
+    }
+}
