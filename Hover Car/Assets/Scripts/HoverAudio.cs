fbf8703e2d700d2e0fa32f9e0ae371c4955ce7a6
kandamashiro <641047675@qq.com>
2016-06-30 23:11:56 +0800

    finished hover car

---
+++ Hover Car/Assets/Scripts/HoverAudio.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoverAudio : MonoBehaviour {
+
+    public AudioSource jetSound;
+    private float jetPitch;
+    private const float lowPitch = .1f;
+    private const float highPitch = 2.0f;
+    private const float SpeedToRevs = .01f;
+
+    Vector3 myVelocity;
+    Rigidbody carRigidbody;
+
+    
+	void Awake () {
+        carRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+        myVelocity = carRigidbody.velocity;
+        float forwardSpeed = transform.InverseTransformDirection(myVelocity).z;
+        Debug.Log(myVelocity);
+        Debug.Log(transform.TransformDirection(myVelocity));
+        Debug.Log(transform.InverseTransformDirection(myVelocity));
+        float engineRevs = Mathf.Abs(forwardSpeed) * SpeedToRevs;
+        jetSound.pitch = Mathf.Clamp(engineRevs, lowPitch, highPitch);
+	}
+}
