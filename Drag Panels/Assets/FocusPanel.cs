3b76dc37e8f1c355eb83d6a96597de13aa4ad3b7
kandamashiro <641047675@qq.com>
2016-06-30 11:00:21 +0800

    finished Drag Panels

---
+++ Drag Panels/Assets/FocusPanel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class FocusPanel : MonoBehaviour, IPointerDownHandler {
+
+    private RectTransform rectTransform;
+
+	void Awake () {
+        rectTransform = GetComponent<RectTransform>();	
+	}
+
+    public void OnPointerDown(PointerEventData eventData) {
+        rectTransform.SetAsLastSibling();
+    }
+}
---
+++ Drag Panels/Assets/PanelToggleButton.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelToggleButton: MonoBehaviour {
+
+    public void TogglePanel(GameObject panel) {
+        panel.SetActive(!panel.activeSelf);
+    }
+}
