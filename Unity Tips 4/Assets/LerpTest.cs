847e1cefdae25d71c4c6b433a6dab25d0d8163ca
kandamashiro <641047675@qq.com>
2016-07-06 10:48:26 +0800

    finished Unity Tips 4

---
+++ Unity Tips 4/Assets/LerpTest.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LerpTest : MonoBehaviour {
+
+    public float destination = 2.0f;
+
+	// Update is called once per frame
+	void Update () {
+        //float newPositionX = Mathf.Lerp(transform.position.x, destination, Time.deltaTime);
+        //transform.position = new Vector3(newPositionX, 0, 0);
+        //Debug.Log(Time.deltaTime);
+        Debug.Log(Time.time);
+        float translation = Time.deltaTime * 10;
+        transform.Translate(0, 0, translation);
+    }
+}
