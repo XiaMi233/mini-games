e71999f4f56ac9a9e0b166ccc87822114b27fc8f
kandamashiro <641047675@qq.com>
2016-07-11 15:13:06 +0800

    finished Scene selection menu

---
+++ Scene selection menu/Assets/Scripts/ChangeLevelAsync.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeLevelAsync : MonoBehaviour {
+
+    public GameObject loading;
+    public Slider loadingBar;
+
+    private AsyncOperation async;
+
+    public void ClickAsync(string level) {
+        loading.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }
+
+    IEnumerator LoadLevelWithBar(string level) {
+        async = SceneManager.LoadSceneAsync(level);
+
+        while (!async.isDone) {
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+}
